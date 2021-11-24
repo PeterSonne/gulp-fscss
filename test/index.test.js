ae413d1330951190558f22bb895c9951fc02bcbb
Peter Sonne <papa@deep2chill.de>
2021-11-24 19:11:31 +0100

    added basic testing

--- test/index.test.js
+++ test/index.test.js
@@ -2,10 +2,40 @@
 const values = require('./inputs.js');
 // our module
 const gulpFsCss = require('./../index.js');
+// libs
+var assert = require('assert');
+//var es = require('event-stream');
+var File = require('vinyl');
+const { doesNotMatch } = require('assert');
 
-test('Basic urls', () => {
-    values.forEach(e => {
-        // TODO: later...
-        //expect(gulpFsCss(e.options)._transform(Buffer.from(e.input))).toEqual(Buffer.from(e.output))
-    })
+describe('gulp-fscss', function() {
+	describe('buffer test pairs', function() {
+
+		values.forEach(function(e) {
+
+			it('input-output pairs match test', function(done) {
+
+				const fakeFile = new File({
+					contents: Buffer.from(e.input)
+				});
+	
+				const fsCssPlugin = gulpFsCss(e.options);
+		
+				fsCssPlugin.write(fakeFile)
+	
+				fsCssPlugin.once('data', function(file) {
+					assert(file.isBuffer());
+
+					const t = file.contents.toString('utf8')
+					assert.equal(file.contents.toString('utf8'), e.output);
+					done();
+				})
+
+				
+
+			})
+	
+		})
+
+	})
 })
--- test/inputs.js
+++ test/inputs.js
@@ -1,7 +1,14 @@
 module.exports = [
     {
+		// nothing should happen here, since we currently don't support hostnames
         input: `background-image: url("https://local.test/img/space/my-file.jpg")`,
         options: {abs: 2},
-        output: `background-image: url($CMS_REF(media:"my-file", abs:2))`
+        output: `background-image: url("https://local.test/img/space/my-file.jpg")`
+    },
+	{
+		// should work well - let's see...
+        input: `background-image: url("/local.test/img/space/my-file.jpg")`,
+        options: {abs: 2},
+        output: `background-image: url("$CMS_REF(media:"my_file", abs: 2)$")`
     }
 ]
